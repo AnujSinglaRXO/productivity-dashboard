41e124904ea307b6c3ebd97e5488f1e9b9c838c4
Anuj Singla <anuj.singla@rxo.com>
2023-07-19 19:00:49 -0500

    Add styles

--- home.py
+++ home.py
@@ -17,10 +17,12 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
+
 class FilterType(Enum):
     CATEGORICAL = ("is in", "is not in")
     NUMERIC = ("==", "!=", ">", "<", ">=", "<=")
     DATETIME = ("since", "until", "on")
+
 
 if "filters" not in st.session_state:
     st.session_state.filters = []
@@ -28,7 +30,7 @@ filter_collection = []
 
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
-with open("web-app/style.css") as f:
+with open("style/style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
@@ -104,41 +106,81 @@ def make_bar(
 
 def make_monthly_issues_plot(df: pd.DataFrame) -> plt.figure:
     # try:
-        plot_data = df[(df["Started"].notnull())]
-        plot_data = plot_data[["Issue Type", "Started"]].reset_index(drop=True)
-        plot_data["Month"] = plot_data["Started"].apply(lambda x: x.strftime("%Y-%m"))
-        plot_data = plot_data.groupby(["Month", "Issue Type"]).size().reset_index(name="Count")
-        plot_data = plot_data.pivot(index="Month", columns="Issue Type", values="Count").fillna(0)
+    plot_data = df[(df["Started"].notnull())]
+    plot_data = plot_data[["Issue Type", "Started"]].reset_index(drop=True)
+    plot_data["Month"] = plot_data["Started"].apply(lambda x: x.strftime("%Y-%m"))
+    plot_data = (
+        plot_data.groupby(["Month", "Issue Type"]).size().reset_index(name="Count")
+    )
+    plot_data = plot_data.pivot(
+        index="Month", columns="Issue Type", values="Count"
+    ).fillna(0)
 
-        # st.dataframe(plot_data)
+    # st.dataframe(plot_data)
 
-        fig = go.Figure()
+    fig = go.Figure()
 
-        bar_width = 1000000000
-        if "Story" in plot_data.columns:
-            fig.add_bar(x=plot_data.index, y=plot_data["Story"], name="Story", marker_color="limegreen", width=bar_width, offset=-bar_width)
-        if "Task" in plot_data.columns:
-            fig.add_bar(x=plot_data.index, y=plot_data["Task"], name="Task", marker_color="deepskyblue", width=bar_width, offset=-bar_width, base=plot_data["Story"] if "Story" in plot_data.columns else None)
-        if "Defect (Standard)" in plot_data.columns:
-            fig.add_bar(y=plot_data["Defect (Standard)"], x=plot_data.index, name="Defect (Standard)", marker_color="darkgrey", width=bar_width, offset=0)
-        if "Bug" in plot_data.columns:
-            fig.add_bar(x=plot_data.index, y=plot_data["Bug"], name="Bug", marker_color="red", width=bar_width, offset=0, base=plot_data["Defect (Standard)"] if "Defect (Standard)" in plot_data.columns else None)
-
-        fig.update_layout(
-            title="Issue Counts by Month",
-            xaxis_title="Month Started (Moved Away from 'Open' Status)",
-            yaxis_title="Count",
-            xaxis_tickangle=0,
-            legend_title="Issue Type",
-            xaxis_ticktext=[f"{calendar.month_abbr[int(x.split('-')[1])]} {x.split('-')[0][-2:]}" for x in plot_data.index],
-            xaxis_tickvals=plot_data.index,
+    bar_width = 1000000000
+    if "Story" in plot_data.columns:
+        fig.add_bar(
+            x=plot_data.index,
+            y=plot_data["Story"],
+            name="Story",
+            marker_color="limegreen",
+            width=bar_width,
+            offset=-bar_width,
         )
-        
-        return fig
-    
-    # except Exception:
-    #     st.error(f"Graph failed.")
-    #     return None
+    if "Task" in plot_data.columns:
+        fig.add_bar(
+            x=plot_data.index,
+            y=plot_data["Task"],
+            name="Task",
+            marker_color="deepskyblue",
+            width=bar_width,
+            offset=-bar_width,
+            base=plot_data["Story"] if "Story" in plot_data.columns else None,
+        )
+    if "Defect (Standard)" in plot_data.columns:
+        fig.add_bar(
+            y=plot_data["Defect (Standard)"],
+            x=plot_data.index,
+            name="Defect (Standard)",
+            marker_color="darkgrey",
+            width=bar_width,
+            offset=0,
+        )
+    if "Bug" in plot_data.columns:
+        fig.add_bar(
+            x=plot_data.index,
+            y=plot_data["Bug"],
+            name="Bug",
+            marker_color="red",
+            width=bar_width,
+            offset=0,
+            base=plot_data["Defect (Standard)"]
+            if "Defect (Standard)" in plot_data.columns
+            else None,
+        )
+
+    fig.update_layout(
+        title="Issue Counts by Month",
+        xaxis_title="Month Started (Moved Away from 'Open' Status)",
+        yaxis_title="Count",
+        xaxis_tickangle=0,
+        legend_title="Issue Type",
+        xaxis_ticktext=[
+            f"{calendar.month_abbr[int(x.split('-')[1])]} {x.split('-')[0][-2:]}"
+            for x in plot_data.index
+        ],
+        xaxis_tickvals=plot_data.index,
+    )
+
+    return fig
+
+
+# except Exception:
+#     st.error(f"Graph failed.")
+#     return None
 
 # params = (
 #     "Repository",
@@ -158,6 +200,7 @@ def make_monthly_issues_plot(df: pd.DataFrame) -> plt.figure:
 #     "Sub tasks",
 #     "Defects"
 # )
+
 
 def add_filter():
     filter_id = str(uuid.uuid4())
@@ -185,7 +228,7 @@ def generate_filter(filter_id, df):
             operator_options = FilterType.NUMERIC
         case _:
             operator_options = FilterType.CATEGORICAL
-    
+
     filter_operator = filter_cols[1].selectbox(
         "Operator",
         options=operator_options.value,
@@ -227,7 +270,6 @@ def generate_filter(filter_id, df):
     )
 
     return {
-
         "param": filter_param,
         "operator": filter_operator,
         "value": filter_value,
@@ -261,6 +303,7 @@ def filter_df(df: pd.DataFrame) -> pd.DataFrame:
                 df = df[df[param] <= value]
 
     return df
+
 
 # st.sidebar.header("RXO Productivity Dashboard")
 # st.sidebar.button("＋ Filter", on_click=add_filter)
